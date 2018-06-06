5e50c9271a6bbdfdb613e5e7e64e0640def95d59
Candy <pawel6132@gmail.com>
2018-06-06 19:50:46 +0200

    Custom Quest Script - 0.901

--- Data/Script/ScriptCustomQuests.lua
+++ Data/Script/ScriptCustomQuests.lua
@@ -1,15 +1,35 @@
-CustomQuests_SystemSwitch = 1
-
 CustomQuests_QuestList = {}
 
+CustomQuests_QuestStatusTable = {}
+
+CustomQuests_CharacterIndexes = {}
+
+--QuestStatus = 1
+
+--MonsterCount = 2
 
 ScriptLoader_AddOnReadScript("CustomQuests_OnReadScript")
 
 ScriptLoader_AddOnNpcTalk("CustomQuests_OnNpcTalk")
 
+ScriptLoader_AddOnCharacterEntry("CustomQuests_OnCharacterEntry")
+
+ScriptLoader_AddOnCharacterClose("CustomQuests_OnCharacterClose")
+
 
 function CustomQuests_OnReadScript()
 
+	local SQL_ODBC = "MuOnline"
+
+	local SQL_USER = ""
+
+	local SQL_PASS = ""
+
+	SQLConnect(SQL_ODBC,SQL_USER,SQL_PASS)
+	
+	CustomQuest_SystemSwitch = ConfigReadNumber("CustomQuestInfo","SystemSwitch","..\\Data\\Script\\Data\\CustomQuest.ini")
+	
+	
 	local ReadTable = FileLoad("..\\Data\\Script\\Data\\CustomQuests.txt")
 
 	if ReadTable == nil then return end
@@ -90,14 +110,118 @@ function CustomQuests_OnReadScript()
 
 end
 
-function CustomQuests_OnNpcTalk(aIndex,bIndex)
+function CustomQuests_OnCharacterEntry(aIndex)
 
-	if aIndex == 257 then
+	if CustomQuests_SystemSwitch == 1 then
 	
-		NoticeSend(bIndex,0,"Guardian: I'll help you my friend.")
+		local CharacterName = GetObjectName(aIndex)
+
+		CustomQuests_AddCharToTable(CharacterName)
+		
+		
+		
+		
+		
+		--if SQLQuery(string.format("SELECT * FROM Character WHERE Name='%s'",CharacterName)) == 0 or SQLFetch() == 0 then
+
+			--SQLClose()
+			
+			--NoticeSend(aIndex,1,"dupa")
+
+		--else
+
+			--CustomQuests_QuestStatusTable[CharacterName].QuestStatus = SQLGetNumber("CustomQuest")
+		
+			--CustomQuests_QuestStatusTable[CharacterName].MonsterCount = SQLGetNumber("CQMonsterCount")
+		
+			--SQLClose()
+		
+			--NoticeSend(aIndex,1,string.format("Your quest status: " + tostring(CustomQuests_QuestStatusTable[CharacterName].QuestStatus)))
+		
+			--NoticeSend(aIndex,1,string.format("Killed monsters: " + tostring(CustomQuests_QuestStatusTable[CharacterName].QuestStatus)))
+		
+		--end
 	
 	end
+
+end
+
+
+function CustomQuests_OnCharacterClose(aIndex)
+
+end
+
+
+
+
+function CustomQuests_OnNpcTalk(aIndex,bIndex)
+
+	if CustomQuests_SystemSwitch ~= 0 then
 	
-	return 1
+		if GetObjectClass(aIndex) == 257 then
+		
+			NoticeSend(bIndex,1,string.format("Guardian: I'll help you, %s.",GetObjectName(bIndex)))
+			
+			-----------------------------------------------------------------------------------------------------------
+			
+			local CharacterName = GetObjectName(bIndex)
+			
+			CustomQuests_AddCharToTable(CharacterName)
+			
+			
+			
+			--if SQLQuery(string.format("SELECT CustomQuest FROM Character WHERE Name='%s'",GetObjectName(bIndex))) == 0 or SQLFetch() == 0 then
+
+			--	SQLClose()
+			
+			--	NoticeSend(bIndex,1,"dupa")
+
+			--else
+
+				-- TU COS JEST ZJEBANE: CustomQuests_QuestStatusTable[CharacterName].QuestStatus = SQLGetNumber("CustomQuest")
+		
+				-- TU COS JEST ZJEBANE: CustomQuests_QuestStatusTable[CharacterName].MonsterCount = SQLGetNumber("CQMonsterCount")
+
+			--	local QuestStage = SQLGetNumber("CustomQuest")
+				
+			--	SQLClose()
+			
+			--	NoticeSend(bIndex,1,string.format(QuestStage))
+				
+			--end
+	
+		end
+		
+	end
+	
+	return 0
+
+end
+
+function CustomQuests_AddCharToTable(aName)
+
+	--Ladowanie Quest Status postaci jesli jej nie ma w tabeli
+			
+	local CharacterIndex = CustomQuests_CharacterIndexes[aName]
+			
+	if CharacterIndex == nil then
+		
+		--Ladowanie Quest Status postaci jesli jej nie ma w tabeli
+	
+	end
+
+end
+
+function CustomQuests_RemoveCharFromTable(aName)
+
+	--Usuwanie postaci z Tabeli jesli sie wylogowuje
+	
+	local CharacterIndex = CustomQuests_CharacterIndexes[aName]
+			
+	if CharacterIndex ~= nil then
+		
+		--Usuwanie postaci z Tabeli jesli sie wylogowuje
+	
+	end
 
 end
