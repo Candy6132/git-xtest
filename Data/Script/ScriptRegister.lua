a5e7c8025e890280e8754b819e1ea1314a1a0d68
Candy <pawel6132@gmail.com>
2018-06-22 22:14:45 +0200

    Register Skrypt - 1.0

--- Data/Script/ScriptRegister.lua
+++ Data/Script/ScriptRegister.lua
@@ -2,26 +2,27 @@ Register_SystemSwitch = 1
 
 Register_CharTimer = {}
 
-Register_CharTimer[1] = 15
+Register_CharTimer[1] = 30
 
-Register_CharTimer[2] = 15
+Register_CharTimer[2] = 30
 
-Register_CharTimer[3] = 15
+Register_CharTimer[3] = 30
 
-Register_CharTimer[4] = 15
+Register_CharTimer[4] = 30
 
-Register_CharTimer[5] = 15
+Register_CharTimer[5] = 30
 
-Register_CharTimer[6] = 15
+Register_CharTimer[6] = 30
 
-Register_CharTimer[7] = 15
+Register_CharTimer[7] = 30
 
-Register_CharTimer[8] = 15
+Register_CharTimer[8] = 30
 
-Register_CharTimer[9] = 15
+Register_CharTimer[9] = 30
 
-Register_CharTimer[10] = 15
+Register_CharTimer[10] = 30
 
+Register_CountingTime = {}
 
 Register_RegisterCharacterName = {}
 
@@ -49,7 +50,7 @@ Register_RegisterCharacterName[10] = "Register10"
 
 ScriptLoader_AddOnCharacterEntry("Register_OnCharacterEntry")
 
-ScriptLoader_AddOnTimerThread("Rgister_OnTimerThread")
+ScriptLoader_AddOnTimerThread("Register_OnTimerThread")
 
 ScriptLoader_AddOnCharacterClose("Rgister_OnCharacterClose")
 
@@ -69,7 +70,7 @@ function Register_OnReadScript()
 	local SQL_PASS = ""
 
 	SQLConnect(SQL_ODBC,SQL_USER,SQL_PASS)
-
+	
 end
 
 
@@ -87,21 +88,29 @@ function Rgister_OnCommandManager(aIndex,code,arg)
 		
 				if CharacterName == Register_RegisterCharacterName[n] then
 				
-					Argument0 = CommandGetArgNumber(arg,0)
+					local Argument0 = CommandGetArgString(arg,0)
 					
-					Argument1 = CommandGetArgNumber(arg,1)
+					local Argument1 = CommandGetArgString(arg,1)
 					
-					Argument2 = CommandGetArgNumber(arg,2)
+					local Argument2 = CommandGetArgString(arg,2)
+					
+					
+					if string.len(Argument0) < 5 then Argument0 = nil end
+
+					if string.len(Argument1) < 5 then Argument1 = nil end
+					
+					if string.len(Argument2) < 5 then Argument2 = nil end
+					
 				
 					if Argument0 ~= nil and Argument1 ~= nil and Argument2 ~= nil then
 					
-						Register_Execute(aIndex,Argument0,Argument1,Argument2)
+						Register_Execute(aIndex,n,Argument0,Argument1,Argument2)
 						
 						return 1
 			
 					else
 			
-						NoticeSend(aIndex,0,"invalid register data")
+						NoticeSend(aIndex,0,"passowrd and email must have at least 5 character")
 			
 					end
 					
@@ -119,11 +128,17 @@ end
 
 
 
-function Register_Execute(aIndex,Argument0,Argument1,Argument2)
+function Register_Execute(aIndex,CharNumber,Argument0,Argument1,Argument2)
 
 	if SQLQuery(string.format("INSERT INTO MEMB_INFO (memb___id, memb__pwd, memb_name, sno__numb, bloc_code, ctl1_code, mail_chek, mail_addr, appl_days, modi_days, out__days, true_days, fpas_ques, fpas_answ) VALUES ('%s','%s','%s','123456','0','0','0','%s','','','','','','')",Argument0,Argument1,Argument0,Argument2)) == 0 then
 	
 		NoticeSend(aIndex,0,"invalid register data")
+		
+	else
+		
+		NoticeSend(aIndex,0,"ACCOUNT SUCCESFULY CREATED")
+		
+		Register_CharTimer[CharNumber] = 0
 		
 	end
 
@@ -134,7 +149,7 @@ end
 function Register_OnCharacterEntry(aIndex)
 
 	if Register_SystemSwitch == 1 then
-	
+
 		local CharacterName = GetObjectName(aIndex)
 		
 		for n=1,#Register_RegisterCharacterName,1 do
@@ -147,9 +162,12 @@ function Register_OnCharacterEntry(aIndex)
 				
 				NoticeSend(aIndex,0,"TYPE /register <login> <password> <email>")
 				
+				Register_CountingTime[n] = 1
+				
 			end
 			
 		end
+	
 	end
 	
 end
@@ -166,7 +184,9 @@ function Rgister_OnCharacterClose(aIndex)
 		
 			if CharacterName == Register_RegisterCharacterName[n] then
 			
-				Register_CharTimer[n] = 15
+				Register_CharTimer[n] = 30
+				
+				Register_CountingTime[n] = 0
 				
 			end
 			
@@ -184,24 +204,28 @@ function Register_OnTimerThread()
 
 		for n=1,#Register_CharTimer,1 do
 
-			Register_CharTimer[n] = Register_CharTimer[n]-1
-		
 			local RegisterCharIndex = GetObjectIndexByName(Register_RegisterCharacterName[n])
 		
 			if Register_CharTimer[n] <= 0 then
 		
-				UserDisconnect(RegisterCharIndex)
+				UserGameLogout(RegisterCharIndex,0)
 			
-				Register_CharTimer[n] = 15
+				Register_CharTimer[n] = 30
 		
 			else
-		
-				NoticeSend(RegisterCharIndex,1,string.format("You will be disconnecterd in: %d",Register_CharTimer[n]))
-		
+			
+				if Register_CountingTime[n] == 1 then
+
+					Register_CharTimer[n] = Register_CharTimer[n]-1
+					
+					NoticeSend(RegisterCharIndex,1,string.format("You will be disconnecterd in: %d",Register_CharTimer[n]))
+				
+				end
+
 			end
 	
 		end
 	
 	end
-end
 
+end
